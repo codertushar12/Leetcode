7b1a1d18dab4fc46741bac7633bb9ab4dc95ec7f
Tushar <tusharsbauskar2001@gmail.com>
2024-10-12 22:36:49 +0530

    Initial commit

---
+++ 1 - 100/1.two-sum.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+  public:
+    /**
+     * Single parse approach
+     * T = O(N)
+     * S = O(N)
+     * 
+     * N = size of nums array
+     */
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        int complement;
+
+        map<int, int> positions;
+
+        vector<int> result(2, -1);
+
+        for (int i = 0; i < n; i++) {
+            complement = target - nums[i];
+
+            if (positions.find(complement) != positions.end()) {
+                result[0] = positions[complement];
+                result[1] = i;
+
+                return result;
+            } else {
+                positions[nums[i]] = i;
+            }
+        }
+
+        return result;
+    }
+};
+// @lc code=end
